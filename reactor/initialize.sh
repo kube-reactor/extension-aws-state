1bcb146b7a5843257e11f38e095a501e9c6bbfed
Adrian Webb <adrian@webb.sh>
2025-02-26 12:03:14 -0500

    Updating the reactor initialize script variable conditional.

--- reactor/initialize.sh
+++ reactor/initialize.sh
@@ -21,15 +21,15 @@ if [ "${STATE_PROVIDER:-}" == "aws_s3" ]; then
   if [[ ! "$AWS_ACCESS_KEY_ID" ]] || [[ ! "$AWS_SECRET_ACCESS_KEY" ]]; then
     emergency "To provision AWS resources, you must specify AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables"
   fi
-
+  #
+  # State
+  #
   export AWS_STATE_PRIMARY_REGION="${AWS_STATE_PRIMARY_REGION:-"us-east-1"}"
   export AWS_STATE_SECONDARY_REGION="${AWS_STATE_SECONDARY_REGION:-"us-west-1"}"
 
   export AWS_STATE_BUCKET_NAME="${AWS_STATE_BUCKET_NAME:-"${APP_NAME}-${__environment}"}"
   export AWS_STATE_KMS_KEY_ID="${AWS_STATE_KMS_KEY_ID:-}"
-  #
-  # State
-  #
+
   export TF_VAR_bucket_name="$AWS_STATE_BUCKET_NAME"
   export TF_VAR_region="$AWS_STATE_PRIMARY_REGION"
   export TF_VAR_replica_region="$AWS_STATE_SECONDARY_REGION"
