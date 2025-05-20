0b6494020668cd14cd1074b5ad4fc4a0386da8c7
Adrian Webb <adrian@webb.sh>
2025-05-20 15:33:42 -0400

    Refactoring the S3 utility library to better handle IAM user switching.

--- reactor/utilities/aws_s3.sh
+++ reactor/utilities/aws_s3.sh
@@ -1,5 +1,25 @@
 
+function check_aws_state_credentials () {
+  if [[ ! "${AWS_STATE_ACCESS_KEY_ID:-}" ]] || [[ ! "${AWS_STATE_SECRET_ACCESS_KEY:-}" ]]; then
+    emergency "To provision AWS Terraform state, you must specify AWS_STATE_ACCESS_KEY_ID and AWS_STATE_SECRET_ACCESS_KEY environment variables"
+  fi
+}
+
+function check_aws_credentials () {
+  if [[ ! "${AWS_ACCESS_KEY_ID:-}" ]] || [[ ! "${AWS_SECRET_ACCESS_KEY:-}" ]]; then
+    emergency "To provision or access AWS services, you must specify AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables for AWS IAM user: ${AWS_TERRAFORM_USER}"
+  fi
+}
+
 function ensure_remote_state_aws_s3 () {
+  check_aws_state_credentials
+
+  local aws_access_key_id="${AWS_ACCESS_KEY_ID:-}"
+  local aws_secret_access_key="${AWS_SECRET_ACCESS_KEY:-}"
+
+  export AWS_ACCESS_KEY_ID="$AWS_STATE_ACCESS_KEY_ID"
+  export AWS_SECRET_ACCESS_KEY="$AWS_STATE_SECRET_ACCESS_KEY"
+
   if [ ! "$AWS_STATE_KMS_KEY_ID" ]; then
     provisioner_create state "${__aws_state_project_dir}" local
 
@@ -8,15 +28,58 @@ function ensure_remote_state_aws_s3 () {
       "s/AWS_STATE_KMS_KEY_ID\=\"\"/AWS_STATE_KMS_KEY_ID\=\"${AWS_STATE_KMS_KEY_ID}\"/" \
       "${__env_dir}/secret.sh"
   fi
+
+  if [[ ! "$aws_access_key_id" ]] || [[ ! "$aws_secret_access_key" ]]; then
+    if [ ! -f "${__env_dir}/aws.json" ]; then
+      echo "creating access key"
+      aws iam create-access-key --user-name "$AWS_TERRAFORM_USER" >"${__env_dir}/aws.json"
+    fi
+
+    export AWS_ACCESS_KEY_ID="$(jq -r ".AccessKey.AccessKeyId" "${__env_dir}/aws.json")"
+    sed -i -E -e \
+      "s?AWS_ACCESS_KEY_ID\=\"[^\"]*\"?AWS_ACCESS_KEY_ID\=\"${AWS_ACCESS_KEY_ID}\"?" \
+      "${__env_dir}/secret.sh"
+
+    export AWS_SECRET_ACCESS_KEY="$(jq -r ".AccessKey.SecretAccessKey" "${__env_dir}/aws.json")"
+    sed -i -E -e \
+      "s?AWS_SECRET_ACCESS_KEY\=\"[^\"]*\"?AWS_SECRET_ACCESS_KEY\=\"${AWS_SECRET_ACCESS_KEY}\"?" \
+      "${__env_dir}/secret.sh"
+  else
+    export AWS_ACCESS_KEY_ID="$aws_access_key_id"
+    export AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
+  fi
+  check_aws_credentials
 }
 
 function destroy_remote_state_aws_s3 () {
+  check_aws_state_credentials
+
+  export AWS_ACCESS_KEY_ID="$AWS_STATE_ACCESS_KEY_ID"
+  export AWS_SECRET_ACCESS_KEY="$AWS_STATE_SECRET_ACCESS_KEY"
+
   if [ "$AWS_STATE_KMS_KEY_ID" ]; then
+    for access_key_id in $(aws iam list-access-keys --user-name "$AWS_TERRAFORM_USER" --query "AccessKeyMetadata[].AccessKeyId" --output "text"); do
+      aws iam delete-access-key --user-name "$AWS_TERRAFORM_USER" --access-key-id "$access_key_id"
+    done
     provisioner_destroy state "${__aws_state_project_dir}" local
-    sed -i -e \
-      "s/AWS_STATE_KMS_KEY_ID\=\"${AWS_STATE_KMS_KEY_ID}\"/AWS_STATE_KMS_KEY_ID\=\"\"/" \
+    rm -f "${__env_dir}/aws.json"
+
+    unset AWS_STATE_KMS_KEY_ID
+    sed -i -E -e \
+      "s/AWS_STATE_KMS_KEY_ID\=\"[^\"]+\"/AWS_STATE_KMS_KEY_ID\=\"\"/" \
+      "${__env_dir}/secret.sh"
+
+    sed -i -E -e \
+      "s?AWS_ACCESS_KEY_ID\=\"[^\"]+\"?AWS_ACCESS_KEY_ID\=\"\"?" \
+      "${__env_dir}/secret.sh"
+
+    sed -i -E -e \
+      "s?AWS_SECRET_ACCESS_KEY\=\"[^\"]+\"?AWS_SECRET_ACCESS_KEY\=\"\"?" \
       "${__env_dir}/secret.sh"
   fi
+
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
 }
 
 function get_remote_state_aws_s3 () {
